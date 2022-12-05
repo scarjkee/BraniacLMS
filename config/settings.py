1b1d12d7e18fd741308c502ca857691c8b379451
scarjkee <frigtfullscar@mail.ru>
2022-12-05 23:18:43 +0500

    lesson_7 Logging

--- config/settings.py
+++ config/settings.py
@@ -163,3 +163,31 @@ SOCIAL_AUTH_VK_OAUTH2_KEY = 'ur59PAr1F8MeWHkUNLCU'
 SOCIAL_AUTH_VK_OAUTH2_SECRET = 'd809b2ddd809b2ddd809b2dd12db18000bdd809d809b2ddbb8e0f9e210b27c59adba64e'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d) %(message)s"
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE,
+            "formatter": "console",
+        },
+        "console": {"class": "logging.StreamHandler", "formatter": "console"},
+    },
+    "loggers": {
+        "django": {"level": "INFO", "handlers": ["console"]},
+        "mainapp": {
+            "level": "DEBUG",
+            "handlers": ["file"],
+        },
+    },
+}
